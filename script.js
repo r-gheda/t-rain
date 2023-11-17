861e2ec4b000d40da27be9f4cd58e982eb0f582d
TheEmotionalProgrammer <isidorotamassia@gmail.com>
2023-11-17 18:38:49 +0100

    la freccetta per scendere di slide è ora presente su ogni slide. Il codice è stato modificato in modo da poter modificare una sezione senza influenzare le altre.

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add the text label for the Y axis
     svg2.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left + 20)
+        .attr("y", 0 - margin.left )
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
