8a49f28aed327ac4afd0645e6813010879d747e6
rgheda <robertogheda.rg@gmail.com>
2023-12-01 14:31:26 +0100

    path highlighting

--- js/stations-dendrogram.js
+++ js/stations-dendrogram.js
@@ -40,6 +40,7 @@ function create_dendro(station_code)
             .attr("d", linksGenerator)
             .style("fill", 'none')
             .attr("stroke", '#003082')
+            .attr("id", function(d) { return "link-" + d.source.data.name + "-" + d.target.data.name; });
 
 
         // Add a circle for each node.
@@ -51,11 +52,135 @@ function create_dendro(station_code)
                 translate(${d.y})`;
             })
             .append("circle")
-                .attr("r", 2)
+                .attr("r", 3)
                 .style("fill", "#f7c82d")
                 .attr("stroke", "#003082")
                 .style("stroke-width", 2)
-                .attr("id", function(d) { return d.data.name })
+                .attr("id", function(d) { return "node-" + d.data.name; })
+                .on("mouseover", function(event, d) {
+                    d3.selectAll("circle")
+                        .transition()
+                        .duration(250)
+                        .style("opacity", 0.2)
+                        .attr("r", 3);
+        
+                    d3.select("#node-" + d.data.name)
+                        .transition()
+                        .duration(250)
+                        .style("opacity", 1)
+                        .attr("r", 5);
+                    
+                    // create a text label for the station
+                    svg.append("text")
+                        .text(d.data.name)
+                        .attr("x", 0)
+                        .attr("y", 0)
+                        .attr("text-anchor", "middle")
+                        .attr("font-size", "12px")
+                        .attr("id", "label-" + d.data.name)
+                        .attr("opacity", 0)
+                        .style("fill", "#003082")
+                        .attr("transform", function() { 
+                            // rotate labels to be horizontal                            
+                            return "rotate(" + (d.x-90) + ") translate(" + d.y + ") rotate(" + (90 + (-d.x)) + ") translate(0, 25)";
+                        });
+
+                    d3.select("#label-" + d.data.name)
+                        .transition()
+                        .duration(250)
+                        .attr("opacity", 1)
+                        .attr("font-size", "14px")
+                        .attr("font-weight",700);
+
+                    d3.selectAll("path")
+                        .transition()
+                        .duration(250)
+                        .style("opacity", 0.2);
+                    
+                    // select the parent node
+                    let parent = d.parent;
+                    let child = d;
+                    while (parent != null)
+                    {
+                        d3.select("#node-" + parent.data.name)
+                            .transition()
+                            .duration(250)
+                            .style("opacity", 1.0)
+                            .attr("r", 5);
+
+                        d3.select("#link-" + parent.data.name + "-" + child.data.name)
+                            .transition()
+                            .duration(250)
+                            .style("opacity", 1);
+
+                        svg.append("text")
+                            .text(child.data.name)
+                            .attr("x", 0)
+                            .attr("y", 0)
+                            .attr("text-anchor", "middle")
+                            .attr("font-size", "12px")
+                            .attr("id", "label-" + child.data.name)
+                            .attr("opacity", 0)
+                            .style("fill", "#003082")
+                            .attr("transform", function() { 
+                                // rotate labels to be horizontal                            
+                                return "rotate(" + (child.x-90) + ") translate(" + child.y + ") rotate(" + (90 + (-child.x)) + ") translate(0, 25)";
+                            })
+                            .raise();
+                        
+                        d3.select("#label-" + child.data.name)
+                            .transition()
+                            .duration(250)
+                            .attr("opacity", 1)
+                            .attr("font-size", "12px")
+                            .attr("font-weight",700);
+                        
+                        child = parent;
+                        parent = parent.parent;
+                    }
+
+
+                    svg.append("text")
+                        .text(child.data.name)
+                        .attr("x", 0)
+                        .attr("y", 0)
+                        .attr("text-anchor", "middle")
+                        .attr("font-size", "12px")
+                        .attr("id", "label-" + child.data.name)
+                        .attr("opacity", 0)
+                        .style("fill", "#003082")
+                        .attr("transform", function() { 
+                            // rotate labels to be horizontal                            
+                            return "rotate(" + (child.x-90) + ") translate(" + child.y + ") rotate(" + (90 + (-child.x)) + ") translate(0, 25)";
+                        });
+                
+                    d3.select("#label-" + child.data.name)
+                        .transition()
+                        .duration(250)
+                        .attr("opacity", 1)
+                        .attr("font-size", "12px")
+                        .attr("font-weight",700);
+
+                })
+                .on("mouseout", function(event, d) {
+                    d3.selectAll("circle")
+                        .style("opacity", 1)
+                        .transition()
+                        .duration(250)
+                        .attr("r", 3);
+
+                    d3.selectAll("path")
+                        .transition()
+                        .duration(250)
+                        .style("opacity", 1);
+                    
+                    // remove the text label
+                    d3.selectAll("text")
+                        .transition()
+                        .duration(250)
+                        .attr("opacity", 0)
+                        .remove();
+                });
 
         // Highlight the root node
         svg.select("#" + station_code)
@@ -68,31 +193,31 @@ function create_dendro(station_code)
         text_labels = false;
 
         // Add the labels
-        svg.selectAll("g")
-            .data(root.descendants())
-            .join("g")
-            .attr("transform", function(d) {
-                return `rotate(${d.x-90})
-                translate(${d.y})`;
-            })
-            .append("text")
-                .text(function(d) { return (d.data.name)})
-                .attr("dy", "0.31em")
-                .attr("x", function(d) { return d.x < 180 === !d.children ? 9 : -9; })
-                .style("font-size", "12px")
-                .style("fill", "red")
-                .style("opacity", 0)
-                .attr("text-anchor", function(d)  { return d.x < 180 === !d.children ? "start" : "end"; })
-                .attr("transform", function(d) { 
-                    let angle = d.x / 180 * Math.PI;
-                    let rad = d.y;
-                    let y = rad * Math.cos(angle);
-                    let x = rad * Math.sin(angle);
-                    if (x === 0 && y === 0) {
-                        return "rotate(" + -90 + ")";
-                    }
-                    return "rotate(" + (x < 0 ? 180 : 0) + ")";
-                });
+        // svg.selectAll("g")
+        //     .data(root.descendants())
+        //     .join("g")
+        //     .attr("transform", function(d) {
+        //         return `rotate(${d.x-90})
+        //         translate(${d.y})`;
+        //     })
+        //     .append("text")
+        //         .text(function(d) { return (d.data.name)})
+        //         .attr("dy", "0.31em")
+        //         .attr("x", function(d) { return d.x < 180 === !d.children ? 9 : -9; })
+        //         .style("font-size", "12px")
+        //         .style("fill", "003082")
+        //         .style("opacity", 0)
+        //         .attr("id", function(d) { console.log("label-" + d.data.name); return ("label-" + d.data.name); })
+        //         .attr("text-anchor", function(d)  { return d.x < 180 === !d.children ? "start" : "end"; })
+        //         .attr("transform", function(d) { 
+        //             // rotate labels to be horizontal
+        //             let angle = d.x / 180 * Math.PI;
+        //             let rad = d.y;
+        //             let y = rad * Math.cos(angle);
+        //             let x = rad * Math.sin(angle);
+                    
+        //             return "rotate(" + (90 + (-angle * 180 / Math.PI)) + ")";
+        //         })
             
     });
 }
@@ -110,19 +235,18 @@ d3.select('#dendro-station-highlight-search').on('keydown', function(e)
     if (e.key === 'Enter')
     {
         let searchValue = this.value.toUpperCase();
-        svg.select("#" + searchValue)
+        svg.select("#node-" + searchValue)
             .attr("r", 5)
             .style("fill", "darkred")
             .attr("stroke", "darkred")
     }
 });
 
-d3.select("#show-dendro-labels-button").on("click", function() {
-    if (text_labels) {
-        svg.selectAll("text").style("opacity", 0);
-        text_labels = false;
-    } else {
-        svg.selectAll("text").style("opacity", 1);
-        text_labels = true;
-    }
+d3.select("#dendro-highlighting-clear-button").on("click", function() {
+    svg.selectAll("circle")
+        .attr("r", 3)
+        .style("fill", "#f7c82d")
+        .attr("stroke", "#003082")
+        .style("stroke-width", 2);
+
 });
